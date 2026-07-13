3bb7398d76f4f2e7ad708776c8d01e898e061ddf
DUM2009 <danielaumatola@gmail.com>
2026-07-13 20:17:11 +0200

    versão 3.6

---
+++ firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBaU8XMtVsSTfkioEH_Vg0Vs6OjTPb7p9U",
+  authDomain: "explore-436a5.firebaseapp.com",
+  projectId: "explore-436a5",
+  storageBucket: "explore-436a5.firebasestorage.app",
+  messagingSenderId: "733101974683",
+  appId: "1:733101974683:web:55ef5278ff5f97639b123e",
+  measurementId: "G-W1NNE6QYJ3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
--- script.js
+++ script.js
@@ -21,3 +21,25 @@ history.back();
 });
 
 }
+function criarConta(){
+
+const email = document.querySelector("#email").value;
+const password = document.querySelector("#password").value;
+
+firebase.auth().createUserWithEmailAndPassword(email, password)
+
+.then(() => {
+
+alert("Conta criada com sucesso!");
+
+window.location.href="login.html";
+
+})
+
+.catch((erro)=>{
+
+alert(erro.message);
+
+});
+
+}
