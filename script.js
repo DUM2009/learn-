dad502e0a84b043584ee7b575607ae5e5f280b3c
DUM2009 <danielaumatola@gmail.com>
2026-07-13 21:44:49 +0200

    versºao 1.0

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-alert("JavaScript carregou!");
 import { auth } from "./firebase.js";
 import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
