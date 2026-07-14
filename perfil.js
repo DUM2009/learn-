87855d56745c362820494c87e81eabfae668b8ff
DUM2009 <danielaumatola@gmail.com>
2026-07-14 19:22:37 +0200

    visão 1.0

--- perfil.js
+++ perfil.js
@@ -27,3 +27,49 @@ logoutBtn.addEventListener("click",()=>{
     signOut(auth);
 
 });
+import { auth } from "./firebase.js";
+
+import {
+
+onAuthStateChanged,
+signOut
+
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+
+const nomeAluno = document.getElementById("nomeAluno");
+const emailAluno = document.getElementById("emailAluno");
+
+const logoutBtn = document.getElementById("logoutBtn");
+
+
+onAuthStateChanged(auth,(user)=>{
+
+    if(user){
+
+        nomeAluno.textContent=user.email.split("@")[0];
+
+        emailAluno.textContent=user.email;
+
+    }
+
+    else{
+
+        window.location.href="login.html";
+
+    }
+
+});
+
+
+logoutBtn.addEventListener("click",()=>{
+
+    signOut(auth)
+
+    .then(()=>{
+
+        window.location.href="login.html";
+
+    });
+
+});
